b5d49be4a0ac8e46e9f406be2db665be3cca2b5f
John Lu <69166028+john-courter-lu@users.noreply.github.com>
2023-12-23 10:17:36 -0600

    Create ASCIIArt.cs

---
+++ ASCIIArt.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      // ASCII art 
+      Console.WriteLine(" .-.");    
+      Console.WriteLine("(o o)");
+      Console.WriteLine(" \\ 0 \\"); // backslash is an escape character
+      Console.WriteLine("  \\   \\"); // so \\ will show up as \
+      Console.WriteLine("   '~~~'"); // space makes a difference
+      Console.WriteLine("Spiro the Specter");
+     }
+  }
+}
